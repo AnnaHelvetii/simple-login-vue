5fa4fe1a07896ae433fdc4a0120e616183e32a40
Anna Soboleva <sithhappens1138@gmail.com>
2024-12-08 01:45:44 +0200

    new fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-	publicPath: '/simple-login-vue/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/simple-login-vue/' : '/',
 	transpileDependencies: true,
 	configureWebpack: {
 		plugins: [
